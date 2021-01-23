e5b7bb2b59b08e366c19f4fe72b4db917f67b9a9
dy1ng <dying@xakep.ru>
2021-01-23 18:17:07 +0300

    Fix main build.gradle.kts --- Add repositories that were missing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.github.rodm.teamcity-common") version "1.1" apply false
-    id("com.github.rodm.teamcity-agent") version "1.1" apply false
-    id("com.github.rodm.teamcity-server") version "1.1" apply false
+    id("com.github.rodm.teamcity-common") version "1.3" apply false
+    id("com.github.rodm.teamcity-agent") version "1.3" apply false
+    id("com.github.rodm.teamcity-server") version "1.3" apply false
 
     kotlin("jvm") version "1.4.10"
 }
@@ -13,6 +13,10 @@ ext {
 group = "jetbrains.buildserver"
 version = System.getenv("BUILD_NUMBER") ?: "1.0-dev"
 
+repositories {
+    mavenCentral()
+    jcenter()
+}
 
 subprojects {
 
